616990a6f3040a20d593a2dc81abf3a6aa24d6fe
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2023-10-20 00:38:12 +0300

    pagination

---
+++ jsoString.js
@@ -0,0 +1,59 @@
+const js = `{
+    "arrayProducts": [],
+    "items": [
+        {
+            "name": "Idemitsu",
+            "img": "Files/oil-idemitsu.png",
+            "price": "3969",
+            "description": "Моторное масло Idemitsu Gasoline & Diesel Fully-Synthetic SN/CF 30015048-746 5W40 4"
+        },
+        {
+            "name": "Лукойл",
+            "img": "Files/oil-lukoil.png",
+            "price": "3999",
+            "description": "Моторное масло Lukoil Genesis Armortech 5W-40 4л. 3148675"
+        },
+        {
+            "name": "Sintec",
+            "img": "Files/oil-sintec.png",
+            "price": "1684",
+            "description": "Моторное масло SINTEC PLATINUM SAE 5W-30 API SL, ACEA A5/B5 Синтетическое 4 л"
+        },
+        {
+            "name": "Zic",
+            "img": "Files/oil-zic.png",
+            "price": "1130",
+            "description": "Моторное масло ZIC X9 5W40 синтетика 4 л 162000"
+        },
+        {
+            "name": "Газпромнефть",
+            "img": "Files/oil-gazprom.png",
+            "price": "1800",
+            "description": "Моторное масло Gazpromneft Premium N 5W-40, 4л"
+        },
+        {
+            "name": "Quartz",
+            "img": "Files/oil-quarts.png",
+            "price": "1599",
+            "description": "Моторное масло QUARTZ FUTURE NFC 9000 5W30 синт.1л TOTAL"
+        },
+        {
+            "name": "Zic",
+            "img": "Files/oil-zic1.png",
+            "price": "2199",
+            "description": "Моторное масло ZIC Х5 10W40 4 л"
+        },
+        {
+            "name": "Kixx",
+            "img": "Files/oil-kixx.png",
+            "price": "3203",
+            "description": "Моторное масло Kixx G1 SP 5W30 4 л, L215344TE1"
+        },
+        {
+            "name": "Quartz",
+            "img": "Files/oil-elf.png",
+            "price": "5190",
+            "description": "Моторное масло Elf Evolution 900 SXR 194878 5W40 4 л"
+        }
+    ]
+}`;
--- menu.js
+++ menu.js
@@ -1,27 +1,76 @@
 
-const prod = document.querySelectorAll('.title-describtion');
-prod.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        document.documentElement.style.cursor = "pointer";
-    });
-    element.addEventListener('mouseout', () => {
-        document.documentElement.style.cursor = "default";
-    });
-    element.addEventListener('click', () => {
-        location.href = 'Product.html';
-    });
-});
+const js = `{
+    "arrayProducts": [],
+    "items": [
+        {
+            "name": "Idemitsu",
+            "img": "Files/oil-idemitsu.png",
+            "price": "3969",
+            "description": "Моторное масло Idemitsu Gasoline & Diesel Fully-Synthetic SN/CF 30015048-746 5W40 4"
+        },
+        {
+            "name": "Лукойл",
+            "img": "Files/oil-lukoil.png",
+            "price": "3999",
+            "description": "Моторное масло Lukoil Genesis Armortech 5W-40 4л. 3148675"
+        },
+        {
+            "name": "Sintec",
+            "img": "Files/oil-sintec.png",
+            "price": "1684",
+            "description": "Моторное масло SINTEC PLATINUM SAE 5W-30 API SL, ACEA A5/B5 Синтетическое 4 л"
+        },
+        {
+            "name": "Zic",
+            "img": "Files/oil-zic.png",
+            "price": "1130",
+            "description": "Моторное масло ZIC X9 5W40 синтетика 4 л 162000"
+        },
+        {
+            "name": "Газпромнефть",
+            "img": "Files/oil-gazprom.png",
+            "price": "1800",
+            "description": "Моторное масло Gazpromneft Premium N 5W-40, 4л"
+        },
+        {
+            "name": "Quartz",
+            "img": "Files/oil-quarts.png",
+            "price": "1599",
+            "description": "Моторное масло QUARTZ FUTURE NFC 9000 5W30 синт.1л TOTAL"
+        },
+        {
+            "name": "Zic",
+            "img": "Files/oil-zic1.png",
+            "price": "2199",
+            "description": "Моторное масло ZIC Х5 10W40 4 л"
+        },
+        {
+            "name": "Kixx",
+            "img": "Files/oil-kixx.png",
+            "price": "3203",
+            "description": "Моторное масло Kixx G1 SP 5W30 4 л, L215344TE1"
+        },
+        {
+            "name": "Quartz",
+            "img": "Files/oil-elf.png",
+            "price": "5190",
+            "description": "Моторное масло Elf Evolution 900 SXR 194878 5W40 4 л"
+        }
+    ]
+}`;
+const jsonObj = JSON.parse(js);
 
 // пагинация
 
 const paginationNumbers = document.getElementById("pagination-numbers");
 const paginatedList = document.getElementById("paginated-list");
 const listItems = paginatedList.querySelectorAll("li");
+const listItemsInArray = jsonObj["items"];
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 
 const paginationLimit = 3;
-const pageCount = Math.ceil(listItems.length / paginationLimit);
+const pageCount = Math.ceil(listItemsInArray.length / paginationLimit);
 let currentPage;
 
 const disableButton = (button) => {
@@ -82,13 +131,47 @@ const setCurrentPage = (pageNum) => {
 
     const prevRange = (pageNum - 1) * paginationLimit;
     const currRange = pageNum * paginationLimit;
+    paginatedList.innerHTML = '';
+    listItemsInArray.forEach((item, index) => {
+        const el = document.createElement("li");
 
-    listItems.forEach((item, index) => {
-        item.classList.add("hidden");
+        el.innerHTML = `<li>
+        <div class="row">
+          <div class="icon-product">
+            <img src="${jsonObj["items"][index]["img"]}">
+            <p>${jsonObj["items"][index]["price"]} Руб.</p>
+          </div>
+          <div class="describtion">
+            <p class="title-describtion">${jsonObj["items"][index]["description"]}</p>
+          </div>
+        </div>
+      </li>`;
+        const prod = document.querySelectorAll('.title-describtion');
+        prod.forEach(element => {
+            element.addEventListener('mouseover', () => {
+                document.documentElement.style.cursor = "pointer";
+            });
+            element.addEventListener('mouseout', () => {
+                document.documentElement.style.cursor = "default";
+            });
+            element.addEventListener('click', () => {
+                location.href = 'Product.html';
+            });
+        });
+
+        el.classList.add("hidden");
         if (index >= prevRange && index < currRange) {
-            item.classList.remove("hidden");
+            el.classList.remove("hidden");
+            paginatedList.appendChild(el);
         }
     });
+
+    // listItems.forEach((item, index) => {
+    //     item.classList.add("hidden");
+    //     if (index >= prevRange && index < currRange) {
+    //         item.classList.remove("hidden");
+    //     }
+    // });
 };
 
 window.addEventListener("load", () => {
@@ -115,31 +198,3 @@ window.addEventListener("load", () => {
 });
 
 // конец пагинации
-
-// const req = new XMLHttpRequest();
-// req.onreadystatechange = function () {
-//     onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             var myArr = JSON.parse(this.responseText);
-//             myFunction(myArr);
-//         }
-//     };
-// };
-// req.open('GET', 'korzina.json', true);
-// req.send();
-
-// function myFunction(arr) {
-//     var out = "";
-//     var i;
-//     for (i = 0; i < arr.length; i++) {
-//         out += '<a href="' + arr[i].url + '">' + arr[i].display + '</a><br>';
-//     }
-//     document.getElementById("id01").innerHTML = out;
-// }
-fetch("https://api.jsonserve.com/LLEMOZ")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
