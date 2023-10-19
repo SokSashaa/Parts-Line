dc92b81ccf3ddb6a5df795b76c192cb0f7803d9c
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2023-10-20 02:16:19 +0300

    АдаптивПодКаждыйЭлемент

--- menu.js
+++ menu.js
@@ -3,54 +3,63 @@ const js = `{
     "arrayProducts": [],
     "items": [
         {
+            "article": "00001",
             "name": "Idemitsu",
             "img": "Files/oil-idemitsu.png",
             "price": "3969",
             "description": "Моторное масло Idemitsu Gasoline & Diesel Fully-Synthetic SN/CF 30015048-746 5W40 4"
         },
         {
+            "article": "00002",
             "name": "Лукойл",
             "img": "Files/oil-lukoil.png",
             "price": "3999",
             "description": "Моторное масло Lukoil Genesis Armortech 5W-40 4л. 3148675"
         },
         {
+            "article": "00003",
             "name": "Sintec",
             "img": "Files/oil-sintec.png",
             "price": "1684",
             "description": "Моторное масло SINTEC PLATINUM SAE 5W-30 API SL, ACEA A5/B5 Синтетическое 4 л"
         },
         {
+            "article": "00004",
             "name": "Zic",
             "img": "Files/oil-zic.png",
             "price": "1130",
             "description": "Моторное масло ZIC X9 5W40 синтетика 4 л 162000"
         },
         {
+            "article": "00005",
             "name": "Газпромнефть",
             "img": "Files/oil-gazprom.png",
             "price": "1800",
             "description": "Моторное масло Gazpromneft Premium N 5W-40, 4л"
         },
         {
+            "article": "00006",
             "name": "Quartz",
             "img": "Files/oil-quarts.png",
             "price": "1599",
             "description": "Моторное масло QUARTZ FUTURE NFC 9000 5W30 синт.1л TOTAL"
         },
         {
+            "article": "00007",
             "name": "Zic",
             "img": "Files/oil-zic1.png",
             "price": "2199",
             "description": "Моторное масло ZIC Х5 10W40 4 л"
         },
         {
+            "article": "00008",
             "name": "Kixx",
             "img": "Files/oil-kixx.png",
             "price": "3203",
             "description": "Моторное масло Kixx G1 SP 5W30 4 л, L215344TE1"
         },
         {
+            "article": "00009",
             "name": "Quartz",
             "img": "Files/oil-elf.png",
             "price": "5190",
@@ -135,17 +144,16 @@ const setCurrentPage = (pageNum) => {
     listItemsInArray.forEach((item, index) => {
         const el = document.createElement("li");
 
-        el.innerHTML = `<li>
-        <div class="row">
+        el.innerHTML = `<div class="row">
           <div class="icon-product">
             <img src="${jsonObj["items"][index]["img"]}">
             <p>${jsonObj["items"][index]["price"]} Руб.</p>
           </div>
           <div class="describtion">
-            <p class="title-describtion">${jsonObj["items"][index]["description"]}</p>
+            <p class="title-describtion" id=${index}>${jsonObj["items"][index]["description"]}</p>
           </div>
-        </div>
-      </li>`;
+        </div>`;
+
         const prod = document.querySelectorAll('.title-describtion');
         prod.forEach(element => {
             element.addEventListener('mouseover', () => {
@@ -154,17 +162,17 @@ const setCurrentPage = (pageNum) => {
             element.addEventListener('mouseout', () => {
                 document.documentElement.style.cursor = "default";
             });
-            element.addEventListener('click', () => {
-                location.href = 'Product.html';
+            element.addEventListener('click', (e) => {
+                location.href = `Product.html?article=${e.target.id}`;
             });
         });
-
         el.classList.add("hidden");
         if (index >= prevRange && index < currRange) {
             el.classList.remove("hidden");
             paginatedList.appendChild(el);
         }
     });
+
 
     // listItems.forEach((item, index) => {
     //     item.classList.add("hidden");
--- product.js
+++ product.js
@@ -1,3 +1,99 @@
+
+const js = `{
+    "arrayProducts": [],
+    "items": [
+        {
+            "article": "00001",
+            "name": "Idemitsu",
+            "img": "Files/oil-idemitsu.png",
+            "price": "3969",
+            "description": "Моторное масло Idemitsu Gasoline & Diesel Fully-Synthetic SN/CF 30015048-746 5W40 4"
+        },
+        {
+            "article": "00002",
+            "name": "Лукойл",
+            "img": "Files/oil-lukoil.png",
+            "price": "3999",
+            "description": "Моторное масло Lukoil Genesis Armortech 5W-40 4л. 3148675"
+        },
+        {
+            "article": "00003",
+            "name": "Sintec",
+            "img": "Files/oil-sintec.png",
+            "price": "1684",
+            "description": "Моторное масло SINTEC PLATINUM SAE 5W-30 API SL, ACEA A5/B5 Синтетическое 4 л"
+        },
+        {
+            "article": "00004",
+            "name": "Zic",
+            "img": "Files/oil-zic.png",
+            "price": "1130",
+            "description": "Моторное масло ZIC X9 5W40 синтетика 4 л 162000"
+        },
+        {
+            "article": "00005",
+            "name": "Газпромнефть",
+            "img": "Files/oil-gazprom.png",
+            "price": "1800",
+            "description": "Моторное масло Gazpromneft Premium N 5W-40, 4л"
+        },
+        {
+            "article": "00006",
+            "name": "Quartz",
+            "img": "Files/oil-quarts.png",
+            "price": "1599",
+            "description": "Моторное масло QUARTZ FUTURE NFC 9000 5W30 синт.1л TOTAL"
+        },
+        {
+            "article": "00007",
+            "name": "Zic",
+            "img": "Files/oil-zic1.png",
+            "price": "2199",
+            "description": "Моторное масло ZIC Х5 10W40 4 л"
+        },
+        {
+            "article": "00008",
+            "name": "Kixx",
+            "img": "Files/oil-kixx.png",
+            "price": "3203",
+            "description": "Моторное масло Kixx G1 SP 5W30 4 л, L215344TE1"
+        },
+        {
+            "article": "00009",
+            "name": "Quartz",
+            "img": "Files/oil-elf.png",
+            "price": "5190",
+            "description": "Моторное масло Elf Evolution 900 SXR 194878 5W40 4 л"
+        }
+    ]
+}`;
+const jsonObj = JSON.parse(js);
+
+
+window.addEventListener('load', () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('article')) {
+        const index = searchParams.get('article');
+        const lin = jsonObj["items"][index];
+
+        const name = lin["name"].toUpperCase();
+        const nameEl = document.querySelector(".titleH");
+        nameEl.innerHTML = `Моторное масло ${name}`;
+
+        const hrefImg = lin["img"];
+        const img = document.querySelector(".icon-prod");
+        img.style.background = `url('${hrefImg}')`;
+        img.style.backgroundPosition = "center";
+        img.style.backgroundSize = "cover";
+        img.style.textAlign = "center";
+
+        const priceHref = lin["price"];
+        const pr = document.querySelector("#price");
+        pr.innerHTML = `${priceHref} Руб.`;
+    }
+
+});
+
 const korz = document.querySelector('.btn-circle-korz');
 const korzina = document.querySelector('.korzina');
 let count = 0;
@@ -32,4 +128,5 @@ korz.addEventListener('click', () => {
         priceEl.innerText = priceOneEl * count + ' Руб.';
     })
 });
+
 
